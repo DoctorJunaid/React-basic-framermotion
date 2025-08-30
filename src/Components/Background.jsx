e94729194d0fbdfc6dcdab9447791d81208a4924
DoctorJunaid <junaid.aurangzeb1@gmail.com>
2025-08-30 19:47:48 +0500

    1

---
+++ src/Components/Background.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <>
+    <div className='absolute z-2 h-full w-full '>  
+        <nav className='text-center w-full absolute top-[5%] text-zinc-400'>
+        Documents
+      </nav>
+
+        <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter text-zinc-400 font-semibold'>
+        Docs.
+        </h1>
+        </div>
+         
+    </>
+  )
+}
+
+export default Background
---
+++ src/Components/Foreground.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Foreground = () => {
+  return (
+    <div>
+        <div className=" w-full h-full top-0 left-0 z-3 fixed">
+
+    </div>
+    </div>
+  )
+}
+
+export default Foreground
